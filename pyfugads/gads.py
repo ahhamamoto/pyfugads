9d2dc6c9876551fe302fe86cfdbfcac335026a0c
Anderson Hiroshi Hamamoto <anderson.hamamoto@gmail.com>
2016-09-29 14:59:55 -0300

    crossover implemented

--- pyfugads/gads.py
+++ pyfugads/gads.py
@@ -33,8 +33,10 @@ class GADS():
         self.best_fitness[0] = self.fitness[best_idx]
         self.best_chromosome[0] = self.population[best_idx]
 
-        selected = self.selection()
-        print(selected)
+        self.selection()
+        print(self.new_population)
+        self.crossover()
+        print(self.new_population)
 
     def generate_population(self):
         """Generate initial population based on input."""
@@ -63,8 +65,25 @@ class GADS():
     def selection(self):
         """Returns a population of selected chromosomes."""
         selected = [self.tournament() for i in range(self.population_size)]
-        return np.array(selected)
+        self.new_population = np.array(selected)
+
+    def mate(self, parent1, parent2):
+        """Creates two offsprings from parents."""
+        difference = abs(parent2 - parent1)
+        variation = difference / 3.
+        higher = np.max([parent1, parent2])
+        offspring1 = higher - variation
+        offspring2 = higher - (2 * variation)
+        return offspring1, offspring2
+
+    def crossover(self):
+        """Performs crossover on new population."""
+        for i in range(0, self.population_size, 2):
+            chance = np.random.ranf()
+            if chance <= self.crossover_rate:
+                self.new_population[i:i+2] = self.mate(self.new_population[i],
+                                                       self.new_population[i+1])
 
 
 gads = GADS([[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3]],
-            20, 50, 0.9, 0.03, 0.01)
+            10, 50, 0.5, 0.03, 0.01)
