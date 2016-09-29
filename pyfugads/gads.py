bc73396148708ffac0cf025a26f45b51477f99eb
Anderson Hiroshi Hamamoto <anderson.hamamoto@gmail.com>
2016-09-29 14:29:02 -0300

    fitness and selection implemented

--- pyfugads/gads.py
+++ pyfugads/gads.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from math import sqrt
 import numpy as np
 import pandas as pd
 
@@ -18,13 +19,52 @@ class GADS():
         self.mutation_rate = mut_rate
         self.selection_method = selection
 
-        self.generate_population(0)
-        self.generate_population(1)
+        self.run(0)
 
-    def generate_population(self, idx):
+    def run(self, idx):
+        """Runs the gads of a single interval."""
+        self.idx = idx
+        self.best_chromosome = np.zeros(self.population_size)
+        self.best_fitness = np.zeros(self.population_size)
+        self.generate_population()
+        self.population_fitness()
+
+        best_idx = np.argmax(self.fitness)
+        self.best_fitness[0] = self.fitness[best_idx]
+        self.best_chromosome[0] = self.population[best_idx]
+
+        selected = self.selection()
+        print(selected)
+
+    def generate_population(self):
         """Generate initial population based on input."""
-        inferior_limit = np.min(self.dataset[idx])
-        superior_limit = np.max(self.dataset[idx])
-        self.population = np.random.uniform(inferior_limit, superior_limit,
+        self.inferior_limit = np.min(self.dataset[self.idx])
+        self.superior_limit = np.max(self.dataset[self.idx])
+        self.population = np.random.uniform(self.inferior_limit,
+                                            self.superior_limit,
                                             self.population_size)
-        print(self.population)
+
+    def calculate_fitness(self, chromosome):
+        """Calculates the fitness of a chromosome."""
+        distance = 0.0
+        for point in self.dataset[self.idx]:
+            distance += (point - chromosome) ** 2
+        return ((self.inferior_limit+self.superior_limit) / sqrt(distance))
+
+    def population_fitness(self):
+        """Calculates the fitness of a population."""
+        fitness = np.array(list(map(self.calculate_fitness, self.population)))
+        self.fitness = fitness
+
+    def tournament(self):
+        """Selects a chromosome with a tournament."""
+        return np.random.choice(self.population)
+
+    def selection(self):
+        """Returns a population of selected chromosomes."""
+        selected = [self.tournament() for i in range(self.population_size)]
+        return np.array(selected)
+
+
+gads = GADS([[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3]],
+            20, 50, 0.9, 0.03, 0.01)
